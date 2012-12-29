55c44a774a358dd581be5b725cefbf5694c9e67d
Zac Stewart <zgstewart@gmail.com>
2012-12-29 17:36:39 -0500

    Add debug logger

--- lib/tengai.rb
+++ lib/tengai.rb
@@ -1,7 +1,22 @@
 $:.unshift File.dirname(__FILE__)
+
+require 'logger'
+
 require 'tengai/version'
 require 'tengai/client'
 require 'tengai/body'
 
 module Tengai
+  class << self
+    attr_accessor :debug
+    attr_reader :logger
+
+    def log(message)
+      logger.debug { message } if debug
+    end
+  end
+
+  @logger ||= ::Logger.new(STDOUT)
+
+  self.debug = false
 end
