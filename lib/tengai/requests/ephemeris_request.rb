02d4a8247b07c629bb5c290dd32744fcecc6c2d7
Zac Stewart <zgstewart@gmail.com>
2013-02-21 03:41:42 -0500

    Make EphemerisParser support fetching vector tables

--- lib/tengai/requests/ephemeris_request.rb
+++ lib/tengai/requests/ephemeris_request.rb
@@ -7,17 +7,20 @@ module Tengai
     FIRST_OBSERVER_PROMPT = %r{Coordinate center \[ <id>,coord,geo  \] :\s*$}.freeze
     SUBSEQUENT_OBSERVER_PROMPT = %r{Use previous center  \[ cr=\(y\), n, \? \] :\s*$}.freeze
     OBSERVER_PROMPT = Regexp.union(FIRST_OBSERVER_PROMPT, SUBSEQUENT_OBSERVER_PROMPT).freeze
-    START_TIME_PROMPT = %r{Starting UT  \[>=   1900-Jan-04 00:01\] :\s*$}.freeze
-    END_TIME_PROMPT = %r{Ending   UT  \[<=   2100-Jan-02 23:58\] :\s*$}.freeze
+    CONFIRM_OBSERVER_PROMPT = %r{Confirm selected station    \[ y/n \] -->\s*$}.freeze
+    REFERNCE_PLANE_PROMPT = %r{Reference plane \[eclip, frame, body \] :\s*}.freeze
+    START_TIME_PROMPT = %r{Starting (C|U)T .* :\s*$}.freeze
+    END_TIME_PROMPT = %r{Ending \s* (C|U)T .* :\s*$}.freeze
     INTERVAL_PROMPT = %r{Output interval \[ex: 10m, 1h, 1d, \? \] :\s*$}.freeze
     ACCEPT_DEFAULT_OUTPUT_PROMPT = %r{Accept default output \[ cr=\(y\), n, \?\] :\s*$}.freeze
     SELECT_QUANTITIES_PROMPT = %r{Select table quantities \[ <#,#\.\.>, \?\] :\s*$}.freeze
     COMPLETED_PROMPT = %r{>>> Select\.\.\. \[A\]gain, \[N\]ew-case, \[F\]tp, \[K\]ermit, \[M\]ail, \[R\]edisplay, \? :\s*$}.freeze
 
     ANY_PROMPT = Regexp.union(
-      DEFAULT_PROMPT, TABLE_PROMPT, OBSERVER_PROMPT, START_TIME_PROMPT,
-      END_TIME_PROMPT, INTERVAL_PROMPT, ACCEPT_DEFAULT_OUTPUT_PROMPT,
-      SELECT_QUANTITIES_PROMPT, COMPLETED_PROMPT, Client::PROMPT).freeze
+      DEFAULT_PROMPT, TABLE_PROMPT, OBSERVER_PROMPT, CONFIRM_OBSERVER_PROMPT,
+      REFERNCE_PLANE_PROMPT, START_TIME_PROMPT, END_TIME_PROMPT, INTERVAL_PROMPT,
+      ACCEPT_DEFAULT_OUTPUT_PROMPT, SELECT_QUANTITIES_PROMPT, COMPLETED_PROMPT,
+      Client::PROMPT).freeze
 
     TIME_FORMAT = '%Y-%b-%d %H:%M'.freeze
 
@@ -26,6 +29,7 @@ module Tengai
       @body = body.to_s
       @start_time = options[:start_time]
       @stop_time = options[:stop_time]
+      @options = options
       @state = :ready
     end
 
@@ -45,9 +49,23 @@ module Tengai
     end
 
     private
+    def table
+      case @options[:table]
+      when :observer then 'o'
+      when :vector then 'v'
+      when :orbital_elements then 'e'
+      else 'v'
+      end
+    end
+
+    def observer
+      @options[:observer] || '500@0'
+    end
+
     def send_command(command, prompt=ANY_PROMPT)
+      Tengai.log "> #{command}"
       result = @client.cmd('String' => command, 'Match' => prompt) do |data|
-        Tengai.log data
+        Tengai.log '< ' + data
       end
       receive_data(result)
     end
@@ -57,11 +75,15 @@ module Tengai
       when DEFAULT_PROMPT
         send_command('E')
       when TABLE_PROMPT
-        send_command('o')
+        send_command table
       when FIRST_OBSERVER_PROMPT
-        send_command('geo')
+        send_command observer
       when SUBSEQUENT_OBSERVER_PROMPT
         send_command('n')
+      when CONFIRM_OBSERVER_PROMPT
+        send_command('y')
+      when REFERNCE_PLANE_PROMPT
+        send_command 'frame'
       when START_TIME_PROMPT
         send_command(@start_time.strftime(TIME_FORMAT))
       when END_TIME_PROMPT
