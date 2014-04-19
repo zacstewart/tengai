d412d401809d041ccbde64c794d998febdced08b
Zac Stewart <zgstewart@gmail.com>
2014-04-19 19:52:52 -0400

    Namespace prompt constants with module

--- lib/tengai/requests/telnet_ephemeris_request.rb
+++ lib/tengai/requests/telnet_ephemeris_request.rb
@@ -1,33 +1,34 @@
 module Tengai
   class TelnetEphemerisRequest
-    SYSTEM_PROMPT = %r{Horizons>}.freeze
-    DEFAULT_PROMPT = %r{<cr>:\s*$}
-    TABLE_PROMPT = %r{Observe, Elements, Vectors  \[o,e,v,\?\] :\s*$}.freeze
-    FIRST_OBSERVER_PROMPT = %r{Coordinate center \[ <id>,coord,geo  \] :\s*$}.freeze
-    SUBSEQUENT_OBSERVER_PROMPT = %r{Use previous center  \[ cr=\(y\), n, \? \] :\s*$}.freeze
-    OBSERVER_PROMPT = Regexp.union(FIRST_OBSERVER_PROMPT, SUBSEQUENT_OBSERVER_PROMPT).freeze
-    CONFIRM_OBSERVER_PROMPT = %r{Confirm selected station    \[ y/n \] -->\s*$}.freeze
-    REFERNCE_PLANE_PROMPT = %r{Reference plane \[eclip, frame, body \] :\s*}.freeze
-    START_TIME_PROMPT = %r{Starting (C|U)T .* :\s*$}.freeze
-    END_TIME_PROMPT = %r{Ending \s* (C|U)T .* :\s*$}.freeze
-    INTERVAL_PROMPT = %r{Output interval \[ex: 10m, 1h, 1d, \? \] :\s*$}.freeze
-    ACCEPT_DEFAULT_OUTPUT_PROMPT = %r{Accept default output \[ cr=\(y\), n, \?\] :\s*$}.freeze
-    OUTPUT_REFERENCE_FRAME_PROMPT = %r{Output reference frame \[J2000, B1950\] :\s*$}.freeze
-    CORRECTIONS_PROMPT = %r{Corrections \[ 1=NONE, 2=LT, 3=LT\+S \]  :\s*$}.freeze
-    OUTPUT_UNITS = %r{Output units \[1=KM-S, 2=AU-D, 3=KM-D\] :\s*$}.freeze
-    CSV_FORMAT_PROMPT = %r{Spreadsheet CSV format    \[ YES, NO \] :\s*$}.freeze
-    LABEL_CARTESIAN_OUTPUT_PROMPT = %r{Label cartesian output    \[ YES, NO \] :\s*$}.freeze
-    SELECT_OUTPUT_TABLE_TYPE_PROMPT = %r{Select output table type  \[ 1-6, \?  \] :\s*$}.freeze
-    SELECT_QUANTITIES_PROMPT = %r{Select table quantities \[ <#,#\.\.>, \?\] :\s*$}.freeze
-    COMPLETED_PROMPT = %r{>>> Select\.\.\. \[A\]gain, \[N\]ew-case, \[F\]tp, \[K\]ermit, \[M\]ail, \[R\]edisplay, \? :\s*$}.freeze
+    module Prompts
+      SYSTEM = %r{Horizons>}.freeze
+      DEFAULT = %r{<cr>:\s*$}
+      TABLE = %r{Observe, Elements, Vectors  \[o,e,v,\?\] :\s*$}.freeze
+      FIRST_OBSERVER = %r{Coordinate center \[ <id>,coord,geo  \] :\s*$}.freeze
+      SUBSEQUENT_OBSERVER = %r{Use previous center  \[ cr=\(y\), n, \? \] :\s*$}.freeze
+      OBSERVER = Regexp.union(FIRST_OBSERVER, SUBSEQUENT_OBSERVER).freeze
+      CONFIRM_OBSERVER = %r{Confirm selected station    \[ y/n \] -->\s*$}.freeze
+      REFERNCE_PLANE = %r{Reference plane \[eclip, frame, body \] :\s*}.freeze
+      START_TIME = %r{Starting (C|U)T .* :\s*$}.freeze
+      END_TIME = %r{Ending \s* (C|U)T .* :\s*$}.freeze
+      INTERVAL = %r{Output interval \[ex: 10m, 1h, 1d, \? \] :\s*$}.freeze
+      ACCEPT_DEFAULT_OUTPUT = %r{Accept default output \[ cr=\(y\), n, \?\] :\s*$}.freeze
+      OUTPUT_REFERENCE_FRAME = %r{Output reference frame \[J2000, B1950\] :\s*$}.freeze
+      CORRECTIONS = %r{Corrections \[ 1=NONE, 2=LT, 3=LT\+S \]  :\s*$}.freeze
+      OUTPUT_UNITS = %r{Output units \[1=KM-S, 2=AU-D, 3=KM-D\] :\s*$}.freeze
+      CSV_FORMAT = %r{Spreadsheet CSV format    \[ YES, NO \] :\s*$}.freeze
+      LABEL_CARTESIAN_OUTPUT = %r{Label cartesian output    \[ YES, NO \] :\s*$}.freeze
+      SELECT_OUTPUT_TABLE_TYPE = %r{Select output table type  \[ 1-6, \?  \] :\s*$}.freeze
+      SELECT_QUANTITIES = %r{Select table quantities \[ <#,#\.\.>, \?\] :\s*$}.freeze
+      COMPLETED = %r{>>> Select\.\.\. \[A\]gain, \[N\]ew-case, \[F\]tp, \[K\]ermit, \[M\]ail, \[R\]edisplay, \? :\s*$}.freeze
 
-    ANY_PROMPT = Regexp.union(
-      DEFAULT_PROMPT, TABLE_PROMPT, OBSERVER_PROMPT, CONFIRM_OBSERVER_PROMPT,
-      REFERNCE_PLANE_PROMPT, START_TIME_PROMPT, END_TIME_PROMPT, INTERVAL_PROMPT,
-      ACCEPT_DEFAULT_OUTPUT_PROMPT, SELECT_QUANTITIES_PROMPT, COMPLETED_PROMPT,
-      OUTPUT_REFERENCE_FRAME_PROMPT, CORRECTIONS_PROMPT, OUTPUT_UNITS,
-      CSV_FORMAT_PROMPT, LABEL_CARTESIAN_OUTPUT_PROMPT,
-      SELECT_OUTPUT_TABLE_TYPE_PROMPT, SYSTEM_PROMPT).freeze
+      ANY = Regexp.union(
+        DEFAULT, TABLE, OBSERVER, CONFIRM_OBSERVER, REFERNCE_PLANE, START_TIME,
+        END_TIME, INTERVAL, ACCEPT_DEFAULT_OUTPUT, SELECT_QUANTITIES,
+        COMPLETED, OUTPUT_REFERENCE_FRAME, CORRECTIONS, OUTPUT_UNITS,
+        CSV_FORMAT, LABEL_CARTESIAN_OUTPUT, SELECT_OUTPUT_TABLE_TYPE,
+        SYSTEM).freeze
+    end
 
     TIME_FORMAT = '%Y-%b-%d %H:%M'.freeze
 
@@ -76,7 +77,7 @@ module Tengai
       @options.fetch(:observer) { SOLAR_SYSTEM_BARYCENTER }
     end
 
-    def send_command(command, prompt=ANY_PROMPT)
+    def send_command(command, prompt=Prompts::ANY)
       Tengai.log "> #{command}"
       result = @client.cmd('String' => command, 'Match' => prompt) do |data|
         Tengai.log '< ' + data
@@ -86,41 +87,41 @@ module Tengai
 
     def receive_data(data)
       case data
-      when DEFAULT_PROMPT
+      when Prompts::DEFAULT
         send_command 'E'
-      when TABLE_PROMPT
+      when Prompts::TABLE
         send_command table
-      when FIRST_OBSERVER_PROMPT
+      when Prompts::FIRST_OBSERVER
         send_command observer
-      when SUBSEQUENT_OBSERVER_PROMPT
+      when Prompts::SUBSEQUENT_OBSERVER
         send_command 'n'
-      when CONFIRM_OBSERVER_PROMPT
+      when Prompts::CONFIRM_OBSERVER
         send_command 'y'
-      when REFERNCE_PLANE_PROMPT
+      when Prompts::REFERNCE_PLANE
         send_command 'frame'
-      when START_TIME_PROMPT
+      when Prompts::START_TIME
         send_command @start_time.strftime(TIME_FORMAT)
-      when END_TIME_PROMPT
+      when Prompts::END_TIME
         send_command @stop_time.strftime(TIME_FORMAT)
-      when INTERVAL_PROMPT
+      when Prompts::INTERVAL
         send_command @interval.to_s + 'm'
-      when ACCEPT_DEFAULT_OUTPUT_PROMPT
+      when Prompts::ACCEPT_DEFAULT_OUTPUT
         send_command 'n'
-      when OUTPUT_REFERENCE_FRAME_PROMPT
+      when Prompts::OUTPUT_REFERENCE_FRAME
         send_command 'J2000'
-      when CORRECTIONS_PROMPT
+      when Prompts::CORRECTIONS
         send_command '1'
-      when OUTPUT_UNITS
+      when Prompts::OUTPUT_UNITS
         send_command '2'
-      when CSV_FORMAT_PROMPT
+      when Prompts::CSV_FORMAT
         send_command 'YES'
-      when LABEL_CARTESIAN_OUTPUT_PROMPT
+      when Prompts::LABEL_CARTESIAN_OUTPUT
         send_command 'YES'
-      when SELECT_OUTPUT_TABLE_TYPE_PROMPT
+      when Prompts::SELECT_OUTPUT_TABLE_TYPE
         send_command '03'
-      when SELECT_QUANTITIES_PROMPT
+      when Prompts::SELECT_QUANTITIES
         send_command 'B'
-      when COMPLETED_PROMPT
+      when Prompts::COMPLETED
         @data = data
         send_command 'N'
       when Client::PROMPT
