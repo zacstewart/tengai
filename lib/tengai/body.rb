57bd07058a7a12bdb6cbc88abe4cb2bc7bb5d36d
Zac Stewart <zgstewart@gmail.com>
2014-04-19 20:42:11 -0400

    Remove dependence on Virtus from body model

--- lib/tengai/body.rb
+++ lib/tengai/body.rb
@@ -1,13 +1,25 @@
 $:.unshift File.dirname(File.dirname(File.dirname(__FILE__)))
 require 'ext/horizons/body_data_sheet_parser'
-require 'virtus'
+require 'date'
 
 module Tengai
   class Body
-    include Virtus
-    attribute :revised_on, Date
-    attribute :name, String
-    attribute :id, Integer
+    attr_accessor :revised_on, :name, :id
+
+    def initialize(attributes)
+      self.revised_on = attributes.fetch(:revised_on)
+      self.name = attributes.fetch(:name)
+      self.id = attributes.fetch(:id)
+    end
+
+    def revised_on=(date)
+      date = Date.parse(date) if date.respond_to?(:to_str)
+      @revised_on = date
+    end
+
+    def id=(id)
+      @id = Integer(id)
+    end
 
     def self.find(client, body, parser=BodyDataSheetParser)
       data = client.cmd('String' => body.to_s, 'Match' => /<cr>:\s*$/)
